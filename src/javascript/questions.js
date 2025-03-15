53e0bfbad674f384821df8fa442cc3badf823113
htan0134 <htan0134@student.monash.edu>
2025-03-15 12:08:00 +0800

    Reorganize files

---
+++ src/javascript/questions.js
@@ -0,0 +1,7 @@
+// fetch("../questions/questions.csv")
+//     .then((response) => response.text())
+//     .then((text) => console.log(text))
+//     .catch((error) =>
+//         console.error("Error fetching the CSV file:", error),
+//     );
+console.log("HI");
